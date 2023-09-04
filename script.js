a0f68ad4942c8d9eb6e76cc45693ada81c694f16
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-09-04 12:58:08 +0530

    Selected all DOM elements and some classes added to HTML code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict";
 
-// User Data
+// USER DATA
 const account1 = {
   owner: "Anisha Thapar",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
+  interestRate: 1.2,
   pin: 1111,
 };
 
@@ -29,5 +29,42 @@ const account4 = {
   pin: 4444,
 };
 
-// Array of accounts
+// ARRAY OF ACCOUNTS
 const accounts = [account1, account2, account3, account4];
+
+// ELEMENTS TO BE MANIPULATED
+
+// Navbar Elements
+let greeting = document.querySelector(".greeting");
+let username = document.querySelector(".username");
+let password = document.querySelector(".password");
+let loginBtn = document.querySelector(".btn-login");
+
+// Bank information elements
+let currentDate = document.querySelector(".current-date");
+let currentTime = document.querySelector(".current-time");
+let dateTime = document.querySelector(".date-time");
+let balance = document.querySelector(".balance");
+let transactionContainer = document.querySelector(".transactions");
+let transactionType = document.querySelector(".transaction--type");
+let transactionDate = document.querySelector(".transaction--date");
+let transactionAmount = document.querySelector(".amount");
+
+// Action elements
+let transferUser = document.querySelector(".transfer-user");
+let transferAmount = document.querySelector(".transfer-amt");
+let transferBtn = document.querySelector(".btn-transfer");
+
+let loanAmount = document.querySelector(".loan-amt");
+let loanBtn = document.querySelector(".btn-loan");
+
+let closeUser = document.querySelector(".close-user");
+let closePin = document.querySelector(".close-pin");
+let closeBtn = document.querySelector(".btn-close");
+
+// Stats Elements
+let inAmount = document.querySelector(".in-amt");
+let outAmount = document.querySelector(".out-amt");
+let interestAmount = document.querySelector(".int-amt");
+let sortBtn = document.querySelector(".sort");
+let timeLeft = document.querySelector(".time-left");
